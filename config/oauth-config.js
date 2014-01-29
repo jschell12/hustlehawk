3f772e226a3f48e8fd1448a5d2dd851c8eb783d0
Josh Schell <jschell12@gmail.com>
2014-01-28 22:31:28 -0500

    update linkedin oauth/api connection settings

--- config/oauth-config.js
+++ config/oauth-config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	LINKEDIN_API_KEY:"75h1j17lpqt2xj",
 	LINKEDIN_SECRET_KEY:"v9dOKXxpMlE6wneU",
-	LINKEDIN_OAUTH_USER_TOKEN:"26632210-09de-4a1b-b37b-d5d1bdf659c7",
-	LINKEDIN_OAUTH_USER_SECRET:"b1633ba2-63b8-431c-9016-4253bd450418",
-	LINKEDIN_CALLBACK_URL : "http://hustlehawk-beta.schellster.com/auth/linkedin/callback/"
+	LINKEDIN_OAUTH_USER_TOKEN:"ea1eb42b-5fac-4f90-9c7d-e6e8e7b0d77f",
+	LINKEDIN_OAUTH_USER_SECRET:"2b8d6a33-d903-4a23-b57b-990b10186f8c",
+	LINKEDIN_CALLBACK_URL : "http://hustlehawk-beta.schellster.com/auth/linkedin/callback"
 };
