17511817ae66ac36fbf32a531cf15b2971f294d3
Josh Schell <jschell12@gmail.com>
2014-08-17 23:20:11 -0400

    .

--- config/secrets.js
+++ config/secrets.js
@@ -1,43 +1,42 @@
-// App Secrets for accessing Linkedin and Job APIs
-// asdf
+// App Secrets for accessing Linkedin and Job APIs 
 module.exports = { 
         test:{},
         localhost: {
                 linkedin: {
-                        LINKEDIN_API_KEY: "",
-                        LINKEDIN_SECRET_KEY: "",
-                        LINKEDIN_CALLBACK_URL :""  
+                        LINKEDIN_API_KEY: "75omxcayu0gh8t",
+                        LINKEDIN_SECRET_KEY: "iudRSTlu1rTUzGqK",
+                        LINKEDIN_CALLBACK_URL : "http://localhost:4000/auth/linkedin/callback"  
                 },
                 careerbuilder: {
-                        developerKey : ""
+                        developerKey : "WDX954Z6FCN5X40YZLHQ"
                 },
                 careerjet: {},
                 indeed: { 
-                        publisherId : ""
+                        publisherId : "9656496047255481"
                 },
                 linkup: {
-                        apiKey : "",
-                        searchKey: ""
-                },
+                        apiKey : "370055E1B925D46C9D52904CCD41F4B2",
+                        searchKey: "4299F0BBB53C01F7B644FA1CA040C860"
+                },  
                 simplyhired: {}   
         },
         development: {
                 linkedin: {
-                        LINKEDIN_API_KEY: "",
-                        LINKEDIN_SECRET_KEY: "",  
-                        LINKEDIN_CALLBACK_URL : ""
+                        LINKEDIN_API_KEY: "7590eir6timkeg",
+                        LINKEDIN_SECRET_KEY: "QwHBhlBuR43Brjy1",  
+                        LINKEDIN_CALLBACK_URL : "http://hustlehawk.schellster.com/auth/linkedin/callback"
                 },
                 careerbuilder: {
-                        developerKey : ""
+                        developerKey : "WDX954Z6FCN5X40YZLHQ"
                 },
-                careerjet: {},
+                careerjet: {}, 
                 indeed: {
-                        publisherId: ""
+                        publisherId: "9656496047255481"
                 },
                 linkup: {
-                        apiKey : "",
-                        searchKey: ""
+                        apiKey : "370055E1B925D46C9D52904CCD41F4B2",
+                        searchKey: "4299F0BBB53C01F7B644FA1CA040C860"
                 },
                 simplyhired: {}
-        } 
+        }    
 };
