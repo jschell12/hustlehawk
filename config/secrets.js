97818b9b8aff3c668be0cd3121885c36b56f7b19
Josh Schell <jschell12@gmail.com>
2014-08-17 22:19:19 -0400

    updated gitignore

---
+++ config/secrets.js
@@ -0,0 +1,42 @@
+
+module.exports = {
+	localhost: {
+		linkedin: {
+			LINKEDIN_API_KEY:"",
+			LINKEDIN_SECRET_KEY:"",
+			LINKEDIN_CALLBACK_URL : ""	
+		},
+		careerbuilder: {
+			developerKey : ""
+		},
+		careerjet: {},
+		indeed: {
+			publisherId : ""
+		},
+		linkup: {
+			apiKey : "",
+			searchKey: ""			
+		},
+		simplyhired: {}
+	},
+	development: { 
+		linkedin: {
+			LINKEDIN_API_KEY:"",
+			LINKEDIN_SECRET_KEY:"",
+			LINKEDIN_CALLBACK_URL : ""	
+		},
+		careerbuilder: {
+			developerKey : ""
+		},
+		careerjet: {},
+		indeed: {
+			publisherId: ""
+		},
+		linkup: {
+			apiKey : "",
+			searchKey: ""	
+		},
+		simplyhired: {}
+	}
+};
+ 
