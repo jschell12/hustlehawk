d9b1e62dd4c2fd0143f09f02c35da22610078378
Josh Schell <jschell12@gmail.com>
2014-08-17 22:35:58 -0400

    secerets

---
+++ config/secrets.js
@@ -0,0 +1,43 @@
+// App Secrets for accessing Linkedin and Job APIs
+// testiclites 
+module.exports = { 
+        test:{},
+        localhost: {
+                linkedin: {
+                        LINKEDIN_API_KEY: "",
+                        LINKEDIN_SECRET_KEY: "",
+                        LINKEDIN_CALLBACK_URL :""  
+                },
+                careerbuilder: {
+                        developerKey : ""
+                },
+                careerjet: {},
+                indeed: { 
+                        publisherId : ""
+                },
+                linkup: {
+                        apiKey : "",
+                        searchKey: ""
+                },
+                simplyhired: {}   
+        },
+        development: {
+                linkedin: {
+                        LINKEDIN_API_KEY: "",
+                        LINKEDIN_SECRET_KEY: "",  
+                        LINKEDIN_CALLBACK_URL : ""
+                },
+                careerbuilder: {
+                        developerKey : ""
+                },
+                careerjet: {},
+                indeed: {
+                        publisherId: ""
+                },
+                linkup: {
+                        apiKey : "",
+                        searchKey: ""
+                },
+                simplyhired: {}
+        } 
+};
